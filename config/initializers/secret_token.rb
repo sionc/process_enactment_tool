1bca8f93473e9e60003ce8d130413ca8eea8bf09
Todd Sedano <todd.sedano@sv.cmu.edu>
2011-09-23 15:41:24 -0700

    Adding in initial repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PET::Application.config.secret_token = '7c336b8b3fe6f05005640e32bd6d4d2f4e4ba4d62332f5ea4c472fbb42e4d1b5dc8f93a579b68a3b1b41dfa4fa358803cac589e9d26e799d7c381d85459d4ae3'

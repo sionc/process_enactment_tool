c81893c66d595a48cafa0cb6bac227c20a0ea9c9
Craig Hokanson <dultimaterunner@gmail.com>
2011-09-28 18:39:34 -0700

    Adding in initial ability to seed lifecycles, and put populated combo box in create project

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+# ActiveRecord::Base.connection.execute("DELETE FROM lifecycles IF EXISTS") 
+# ActiveRecord::Base.connection.execute("TRUNCATE table lifecycle_phases") 
+
+sql = "INSERT INTO lifecycles ('name', 'description') 
+       VALUES ('Simplified Waterfall', 'This is a simplified version of the waterfall method'),
+              ('Rational Unified Process', 'This is a rational unified process method');"
+       
+ActiveRecord::Base.connection.execute(sql)
+
+       

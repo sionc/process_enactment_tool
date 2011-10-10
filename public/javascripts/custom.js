7a0ef729f8887d9a5cc8e97716169e055214b480
Jason Leng <jason.leng@sv.cmu.edu>
2011-10-09 22:19:34 -0700

    [GD/JL] [View Project] Moved the new deliverable button into the project phase details box. Updated the target of the new deliverable link when the project phases table row click function returns. Hid the project phase id column

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -10,16 +10,22 @@ $(document).ready(function() {
 
     $('#project_phase_details_container').hide();
 
+
     $('#project_phases tbody tr').click(function() {
+        var projectPhaseId = $(this).find(".project_phase_id_cell").text();
+
         $.ajax({
             type:    'GET',
-            url:     '/project_phases/' + $(this).find(".project_phase_id_cell").text(),
+            url:     '/project_phases/' + projectPhaseId,
             dataType: 'json',
             success: function (data) {
                 $('#project_phase_name').text(data.lifecycle_phase.name);
                 $('#project_phase_description').text(data.lifecycle_phase.description);
                 $('#project_phase_sequence_number').text(data.lifecycle_phase.sequence_number);
                 $('#project_phase_estimated_effort').text(data.lifecycle_phase.estimated_effort);
+
+                $('#new_deliverable_button').attr("href", '/deliverables/new?project_phase_id=' + projectPhaseId);
+
                 $('#project_phase_details_container').show();
             },
             error: function() {

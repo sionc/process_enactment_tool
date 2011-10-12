bf94d0e0af43f370849ace90edeed7f899c8dc35
Jason Leng <jason.leng@sv.cmu.edu>
2011-10-12 01:04:20 -0700

    [GD/CH/SC/JL] [View Project] Fixed a jquery bug.

--- public/javascripts/deliverable.js
+++ public/javascripts/deliverable.js
@@ -1,12 +1,14 @@
-$(document).ready(function() {
-   $('#deliverable_assignable_id').change(function() {
+var showHiddenInputField =
+    function() {
        var deliverableTypeId =  $('#deliverable_assignable_id').val();
        var hiddenInputBox = '<input id="deliverable_stock_deliverable_type_id"' +
         ' name="deliverable[stock_deliverable_type_id]" type="hidden" value="' +
         deliverableTypeId +'" />';
 
        $('#deliverable_type_id').append(hiddenInputBox);
+   };
 
-
-   });
+$(document).ready(function() {
+   showHiddenInputField();
+   $('#deliverable_assignable_id').change(showHiddenInputField);
 });

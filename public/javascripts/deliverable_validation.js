0e8b5c4417a1e8f1596c96ce2dc597897621c78a
Craig Hokanson <dultimaterunner@gmail.com>
2011-11-10 17:42:41 -0800

    [CH/SC] [Design Improvement] added in client-side validation for estimated fields, checking that they are a number greater than zero

--- public/javascripts/deliverable_validation.js
+++ public/javascripts/deliverable_validation.js
@@ -5,12 +5,24 @@ $(document).ready(function() {
 		rules: { 
 			"deliverable[name]": "required",
 			
-			"deliverable[estimated_size]": "required",
-			"deliverable[estimated_production_rate]": "required",
-			"deliverable[estimated_effort]": "required"
+			"deliverable[estimated_size]": {
+				required: true,
+				min: 0,
+				number: true 
+			},
+			"deliverable[estimated_production_rate]": {
+				required: true,
+				min: 0,
+				number: true 
+			},
+			"deliverable[estimated_effort]": {
+				required: true,
+				min: 0,
+				number: true 
+			},
 		},
         errorPlacement: function(error, element) {
-
+        
             switch(element.attr("name"))
             {
                 case "deliverable[estimated_size]":

53e213d21fd67ed9fe08ee1d82b49ab8fa5fbb40
Jason Leng <jason.leng@sv.cmu.edu>
2011-11-19 10:13:33 -0800

    [GD/JL] [Log Effort] Fixed failure tests and removed pending tests.

--- spec/views/effort_logs/index.html.erb_spec.rb
+++ spec/views/effort_logs/index.html.erb_spec.rb
@@ -24,9 +24,6 @@ describe "effort_logs/index.html.erb" do
     assert_select "tr>td", :text => 1.to_s, :count => 2
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => 1.to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => 1.to_s, :count => 2
+
   end
 end

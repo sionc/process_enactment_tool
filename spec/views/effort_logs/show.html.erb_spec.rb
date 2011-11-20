9f96a438e551f666b2d86d73d18a03c153616cdd
Jason Leng <jason.leng@sv.cmu.edu>
2011-11-20 11:24:53 -0800

    [SC/JL] [Log Effort] Fixed a failed test.

--- spec/views/effort_logs/show.html.erb_spec.rb
+++ spec/views/effort_logs/show.html.erb_spec.rb
@@ -2,11 +2,22 @@ require 'spec_helper'
 
 describe "effort_logs/show.html.erb" do
   before(:each) do
+    @stock_deliverable_type = Factory.create(:stock_deliverable_type)
+    complexity = Complexity.create(:level => "low")
+    @attr = {:name => "MyString",
+             :description => "MyText",
+             :estimated_size => 1.5,
+             :estimated_production_rate => 2.5,
+             :estimated_effort => 3.75,
+             :complexity_id => complexity.id
+    }
+    @deliverable = @stock_deliverable_type.deliverables.create! @attr
+
     @effort_log = assign(:effort_log, stub_model(EffortLog,
       :interrupt_time => 1,
       :comment => "MyText",
       :user_id => 1,
-      :deliverable_id => 1
+      :deliverable_id => @deliverable.id
     ))
   end
 

8cbf0221da1efcede1cd65536e9e79b87fd69ec8
George Dean <gdean123@gmail.com>
2011-11-26 11:44:10 -0800

    [GD/JL] [Admin User] Added role to user index page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+  
+  # Return a string containing the first user role (if present)
+  def get_user_role(user)
+    roles = user.roles.collect{|r| r.name}
+    if roles.empty?
+      return "(none)"      
+    else
+      return roles[0]
+    end
+  end
+  
 end
